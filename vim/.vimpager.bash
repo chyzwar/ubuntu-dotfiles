7de2f8b60d00ccf1a1d56df5e0538cd343c5b8b1
Chyzwar <schizek.marcin@gmail.com>
2015-09-14 09:30:53 +0100

    vim pager

--- vim/.vimpager.bash
+++ vim/.vimpager.bash
@@ -1,11 +1,11 @@
 #Add vimpager to path
 #Phttps://github.com/rkitover/vimpager
-# export PAGER=/usr/local/bin/vimpager
+export PAGER=/usr/local/bin/vimpager
 
 # #Add vimpager aliases
 # alias less=$PAGER
 # alias zless=$PAGER
-# # alias cat="cat $* | $PAGER" 
+alias cat="cat $* | $PAGER" 
 
 # alias chuj="cat"
 
