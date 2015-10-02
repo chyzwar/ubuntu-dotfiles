67b98396dac604da12809c09b344455d06070de8
Chyzwar <schizek.marcin@gmail.com>
2015-10-02 21:24:18 +0100

    space mac setup

--- setup/emacs-install.bash
+++ setup/emacs-install.bash
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-echo "Install emacs 24"
+tput setaf 1; echo "Install emacs 24"; tput sgr0
 sudo apt-get install emacs-snapshot
 
-echo "Install spacemac"
+tput setaf 1; echo "Install spacemac"; tput sgr0
 git clone --recursive https://github.com/syl20bnr/spacemacs ~/.emacs.d
 
+tput setaf 1; echo "Do you want to install preconfigured .spacemac file"; tput sgr0
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes )
+        	DIR="$( cd "$( dirname "$0" )" && pwd )"	
+			ln -sfv "$DIR"/../emacs/.spacemacs /home/"$USER"/.spacemacs
+			break;;
+        No ) break;;
+    esac
+done
