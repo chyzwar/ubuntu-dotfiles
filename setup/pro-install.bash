0e39f3753ed81e2dfa79efcb550f3bff327be7cf
Chyzwar <schizek.marcin@gmail.com>
2016-01-10 10:31:10 +0000

    suiblime install

--- setup/pro-install.bash
+++ setup/pro-install.bash
@@ -232,7 +232,7 @@ select yn in "Yes" "No"; do
 			# Install Stack
 			sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 575159689BEFB442
 			echo 'deb http://download.fpcomplete.com/ubuntu wily main'| sudo tee /etc/apt/sources.list.d/fpco.list
-			sudo apt-get update
+			sudo apt-get update -qq
 			sudo apt-get install -y stack
 			break;;
 		No ) break;;
