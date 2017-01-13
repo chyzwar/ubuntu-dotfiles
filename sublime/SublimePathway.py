5fc3e3f8ace2f4766bf1c07ff2b856bbaf346097
Chyzwar <schizek.marcin@gmail.com>
2017-01-13 23:08:47 +0000

    pathway

--- sublime/SublimePathway.py
+++ sublime/SublimePathway.py
@@ -4,10 +4,13 @@ import os
 
 CARGO = '/home/raziel/.cargo/bin:'
 RBENV = '/home/raziel/.rbenv/shims:'
+NODENV = '/home/raziel/.nodenv/shims:'
 
 # Add Stuff To Path
 os.environ['PATH'] += ':'
 os.environ['PATH'] += CARGO
 os.environ['PATH'] += RBENV
+os.environ['PATH'] += NODENV
+
 
 print('PATH = ' + os.environ['PATH'])
