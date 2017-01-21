f9416938e2de2f9d50d4a3324d1693ce717c41f8
Chyzwar <schizek.marcin@gmail.com>
2017-01-21 16:25:33 +0000

    fix colours

--- sublime/SublimePathway.py
+++ sublime/SublimePathway.py
@@ -5,12 +5,14 @@ import os
 CARGO = '/home/raziel/.cargo/bin:'
 RBENV = '/home/raziel/.rbenv/shims:'
 NODENV = '/home/raziel/.nodenv/shims:'
+CRENV = '/home/raziel/.crenv/shims:'
 
 # Add Stuff To Path
 os.environ['PATH'] += ':'
 os.environ['PATH'] += CARGO
 os.environ['PATH'] += RBENV
 os.environ['PATH'] += NODENV
+os.environ['PATH'] += CRENV
 
 
 print('PATH = ' + os.environ['PATH'])
