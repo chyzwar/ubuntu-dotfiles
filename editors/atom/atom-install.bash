ebe8c58a9444aa5b413213f09d95e36a169b9988
chyzwar <schizek.marcin@gmail.com>
2018-03-10 23:58:27 +0000

    nix is stil broken

--- editors/atom/atom-install.bash
+++ editors/atom/atom-install.bash
@@ -1,7 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# shellcheck disable=SC1090
 
+DIR="$(dirname "$(readlink -f "$0")")"
+source "$DIR/../install/nix-install.bash"
 
-tput setaf 1; echo "Install Atom Editor"; tput sgr0
-sudo snap install atom --classic
+tput setaf 2; echo "Installing Atom Editor "; tput sgr0
+nix-env --install atom-1.24.0
 
+tput setaf 2; echo "Installing sync-settings "; tput sgr0
 apm install sync-settings
--- editors/vim/vim-install.bash
+++ editors/vim/vim-install.bash
@@ -1,13 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# shellcheck disable=SC1090
 
-DIR="$( cd "$( dirname "$0" )" && pwd )"
+DIR="$(dirname "$(readlink -f "$0")")"
+source "$DIR/../install/nix-install.bash"
 
-echo "Install vim and neovim"
-sudo apt-get install -y vim
+tput setaf 2; echo "Installing vim"; tput sgr0
+nix-env --install vim-8.0.1257
 
 echo "Install vim-plug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 
 echo "Install .vimrc configs"
 ln -sfv "$DIR/.vimrc" "/home/$USER/.vimrc"
