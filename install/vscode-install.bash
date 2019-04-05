b48cbc4d8235e5f48679e3c82fdc3e77ee54d95c
chyzwar <schizek.marcin@gmail.com>
2019-04-05 11:15:34 +0100

    remove vim pager, clear up things

--- install/vscode-install.bash
+++ install/vscode-install.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 tput setaf 2; echo "Install VSCode Editor"; tput sgr0
-sudo snap install --classic vscode
+sudo snap install --classic code
 
 code --install-extension Shan.code-settings-sync
