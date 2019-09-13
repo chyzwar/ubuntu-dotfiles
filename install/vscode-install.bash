9ec56d3f8458519d538da4129eb10751fb906b3b
chyzwar <schizek.marcin@gmail.com>
2019-09-13 22:03:16 +0100

    chore(): add code insdider

--- install/vscode-install.bash
+++ install/vscode-install.bash
@@ -2,5 +2,6 @@
 
 tput setaf 2; echo "Install VSCode Editor"; tput sgr0
 sudo snap install --classic code
+sudo snap install --classic code-insiders
 
 code --install-extension Shan.code-settings-sync
